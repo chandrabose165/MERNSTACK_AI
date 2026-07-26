9d432f30f5c76c2d08d3f090309cc45c531d141e
chandrabose165 <varisibunny@gmail.com>
2026-07-26 12:06:43 +0530

    landing-page updated

---
+++ javascript/LoginForm.js
@@ -0,0 +1,28 @@
+let RollNo = "2211CS030165";
+let studentName = {
+   FirstName : "chandrabose",
+   LastName : "varisa",
+}
+const FatherName = "varisaRamarao";
+let Date_Of_Birth = "01/04/2003";
+let Mobile_No = 7893259524;
+let EmailId = "varisachandrabose20@gmail.com";
+let password = "chandrabose@111";
+let Gender = "Male";
+let Department = "CSE";
+let Course = "Select current coursers";
+let City = "Hyderabad";
+let Address = "Moosapet old village";
+
+
+console.log("RollNo: " + RollNo);
+console.log("FirstName: " + studentName.FirstName + " LastName:" + studentName.LastName);
+console.log("FatherName: "+FatherName);
+console.log("Date_Of_Birth: "+Date_Of_Birth);
+console.log("EmailId:" + EmailId);
+console.log("password : " + password);
+console.log("Gender :"+Gender);
+console.log("Department: " + Department);
+console.log("course: " +Course);
+console.log("city: " +City);
+console.log("Address: " +Address);
