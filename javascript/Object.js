30710ce0638db86ef5743cd24f810c3745176de3
chandrabose165 <varisibunny@gmail.com>
2026-08-04 17:59:57 +0530

    Javascript

---
+++ javascript/Object.js
@@ -0,0 +1,36 @@
+const student = {
+  Fname: "chandrabose",
+  LName: "varisa",
+  age: 23,
+  emailId: "varisachandrabose20@gmail.com",
+  phone: 7893259524,
+};
+// Access the propertie in the object
+console.log("Name: " + student.Fname);
+console.log("age: " + student.age);
+
+// Adding the data in the Object
+student.color = "red";
+console.log(student);
+
+// using loop to print the element in separate line
+for (let i in student) {
+  console.log(i + ":" + student[i] + " -> " + typeof student[i]);
+}
+
+// changing the values in data
+student.age = 25;
+console.log(student);
+
+// delete the specific properties in object using delete method it was
+{
+  const student = {
+    Fname: "chandrabose",
+    LName: "varisa",
+    age: 23,
+    emailId: "varisachandrabose20@gmail.com",
+    phone: 7893259524,
+  };
+  delete student.color;
+  console.log(student);
+}
