5ebea2edcfe3a0153d2dcda27136be53965ec919
chandrabose165 <varisibunny@gmail.com>
2026-08-07 09:10:51 +0530

    javascript

---
+++ javascript/condition_statements.js
@@ -0,0 +1,16 @@
+let Num_1 = 5;
+let Num_2 = 10;
+let opertors = "%";
+if (opertors == "+") {
+  console.log(Num_1 + Num_2);
+} else if (opertors == "-") {
+  console.log(Num_1 - Num_2);
+} else if (opertors == "*") {
+  console.log(Num_1 * Num_2);
+} else if (opertors == "/") {
+  console.log(Num_1 / Num_2);
+} else if (opertors == "%") {
+  console.log(Num_1 % Num_2);
+} else {
+  console.log("No value are printing in the console");
+}
