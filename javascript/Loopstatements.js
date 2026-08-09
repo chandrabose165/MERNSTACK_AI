bb356d395a06c0deca2df7640d539c213b513ce7
chandrabose165 <varisibunny@gmail.com>
2026-08-09 12:40:13 +0530

    javascript

---
+++ javascript/Loopstatements.js
@@ -0,0 +1,50 @@
+// // In the for loop we know the starting and ending point
+// // For loop can be execute in the block code
+// // It is task to until it finished
+// // repeated the element with written manual we can write it loop
+// // help to rduce the code , maintai the code and orginze the code in manner
+
+// // for loop
+// // let n = 5;
+// // for (let i = 1; i <= n; i++) {
+// //   let row = "";
+// //   for (let j = 1; j <= i; j++) {
+// //     row = row + "*";
+// //   }
+// //   console.log(row);
+// // }
+
+// // while loop
+// // we don't know the ending loop until it will stop
+// // let i = 1;
+// // while (i <= 5) {
+// //   console.log(i);
+// //   i++;
+// // }
+
+// // // we can use atleast once in loop
+// // let num = 6;
+// // do {
+// //   console.log(num);
+// //   num++;
+// // } while (num <= 5);
+
+// // for each loop
+// // in for Each we dont enter any ending value it will automatically stop the loop in javascript
+// let items = ["bike", "car", true, 0.5];
+// items.forEach((machine, index) => {
+//   console.log(index, machine);
+// });
+
+// // label statements
+
+// // Label the outer loop
+// for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     if (i === 1 && j === 1) {
+//       // Breaks the outer loop entirely
+//       break;
+//     }
+//     console.log(`i = ${i}, j = ${j}`);
+//   }
+// }
