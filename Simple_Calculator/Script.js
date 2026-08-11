e524ad70a40f59091d54b799873dab47ce6bfc90
chandrabose165 <varisibunny@gmail.com>
2026-08-11 16:01:42 +0530

    calcultor

--- Simple_Calculator/Script.js
+++ Simple_Calculator/Script.js
@@ -1,23 +1,40 @@
-//  task 1
-const ouput = document.querySelectorAll(".btn");
-console.log(ouput);
+// function add(Num1, Num2) {
+//   console.log(Num1 + Num2);
+// }
+// function sub(Num1, Num2) {
+//   console.log(Num1 - Num2);
+// }
+// function Mult(Num1, Num2) {
+//   console.log(Num1 * Num2);
+// }
+// function div(Num1, Num2) {
+//   console.log(Num1 / Num2);
+// }
+// function square(Num1, Num2) {
+//   console.log(Num1 ** Num2);
+// }
+// function MOD(Num1, Num2) {
+//   console.log(Num1 % Num2);
+// }
 
-//  task 2
-function add(Num1, Num2) {
-  console.log(Num1 + Num2);
-}
-function sub(Num1, Num2) {
-  console.log(Num1 - Num2);
-}
-function Mult(Num1, Num2) {
-  console.log(Num1 * Num2);
-}
-function div(Num1, Num2) {
-  console.log(Num1 / Num2);
-}
-function square(Num1, Num2) {
-  console.log(Num1 ** Num2);
-}
-function MOD(Num1, Num2) {
-  console.log(Num1 % Num2);
-}
+let buttons = document.querySelectorAll(".btn");
+let input = document.querySelector("#display");
+let output = document.querySelector("#history");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "C") {
+      input.textContent = "";
+      output.textContent = "";
+    } else if (button.textContent === "=") {
+      try {
+        let res = eval(input.textContent);
+        output.textContent += res;
+      } catch {
+        input.textContent = "error";
+      }
+    } else {
+      input.textContent += button.textContent;
+    }
+  });
+});
